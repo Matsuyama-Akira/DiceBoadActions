85ae318f2735ef231bd30164578cd99fd41f2d7f
MatsuyamaAkira <matsuyama220061@eggnet.ac.jp>
2023-12-13 16:31:45 +0900

    2023.12.13 (Add)Comment

--- Assets/MainGameFolder/Script/Battle/DamageUI.cs
+++ Assets/MainGameFolder/Script/Battle/DamageUI.cs
@@ -4,32 +4,42 @@ using TMPro;
 
 public class DamageUI : MonoBehaviour
 {
-
+    /// <summary> プレイヤーのカメラのトランスフォーム </summary>
+    private Transform playerCam;
+    /// <summary> ダメージ量のテキスト </summary>
     private TextMeshProUGUI damageText;
-    //　フェードアウトするスピード
+    /// <summary> フェードアウトの速度 </summary>
     private float fadeOutSpeed = 1f;
-    //　移動値
+    /// <summary> フェードアウト中の移動速度 </summary>
     [SerializeField]
     private float moveSpeed = 0.4f;
 
+    /// <summary>
+    /// セットアップ
+    /// </summary>
     private void Awake()
     {
+        // ワールドカメラにプレイヤーカメラを入れて画面に表示できるように
         GetComponent<Canvas>().worldCamera =
             GameObject.FindWithTag("Player").GetComponentInChildren<Camera>();
-    }
 
-    void Start()
-    {
+        // プレイヤーカメラをキャッシュ
+        playerCam = Camera.main.transform;
+
+        // テキストをキャッシュ
         damageText = GetComponentInChildren<TextMeshProUGUI>();
     }
 
     void LateUpdate()
     {
-        transform.rotation = Camera.main.transform.rotation;
+        // UIの挙動
+        transform.rotation = playerCam.rotation;
         transform.position += Vector3.up * moveSpeed * Time.deltaTime;
 
+        // 徐々に透明に
         damageText.color = Color.Lerp(damageText.color, new Color(1f, 0f, 0f, 0f), fadeOutSpeed * Time.deltaTime);
 
+        // テキストのアルベドが0.1を切ったらUIを消滅させる
         if (damageText.color.a <= 0.1f)
         {
             Destroy(gameObject);
--- Assets/MainGameFolder/Script/Battle/Player/PlayerController.cs
+++ Assets/MainGameFolder/Script/Battle/Player/PlayerController.cs
@@ -3,44 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(PlayerStatus))]
 public class PlayerController : MonoBehaviour
 {
     [Header("Asset")]
-    [SerializeField] Animator animator;
-    [SerializeField] PlayerStatus status;
+    [SerializeField, Tooltip("アニメーターを挿入")] Animator animator;
+    [SerializeField, Tooltip("PlayerStatusを挿入")] PlayerStatus status;
 
     [Space, Header("Movement Status")]
-    [SerializeField, Range(1, 10)] private float speed = 5;
-    [SerializeField, Range(1f, 100f)] float jumpStrength;
-    [SerializeField, Range(0.1f, 1f)] float jumpMove;
-    public event System.Action Jumped;
-    public event System.Action Attacked;
-    public event System.Action UniqueAttacked;
-    [SerializeField, Range(2, 20)] float runSpeed = 9;
+    [SerializeField, Range(1, 10), Tooltip("歩行の移動速度")] private float speed = 5;
+    [SerializeField, Range(1f, 100f), Tooltip("ジャンプの高さ")] float jumpStrength;
+    [SerializeField, Range(0.1f, 1f), Tooltip("空中の移動速度")] float jumpMove;
+    [SerializeField, Range(2, 20), Tooltip("ダッシュの移動速度")] float runSpeed = 9;
 
-    //private states
-    /// <summary> Amount of diagonal movement </summary>
+    // プライベートのステータス
+    /// <summary> 斜め移動の倍率 </summary>
     private float sqrtMove = (float)(1 / Math.Sqrt(2));
-    /// <summary> Amount of movement in front or directly behind </summary>
+    /// <summary> 横移動の倍率 </summary>
     public float moveX { get; private set; }
-    /// <summary> Amount of movement in right or left </summary>
+    /// <summary> 縦移動の倍率 </summary>
     public float moveY { get; private set; }
-    /// <summary> Now play animation </summary>
+    /// <summary> 再生中のアニメーション名 </summary>
     private string nowAnim;
-    /// <summary>  </summary>
 
+    /// <summary> 再生中のアニメーション </summary>
     private AnimatorClipInfo[] clipInfos;
 
-    Rigidbody rigidbody;
-    /// <summary> Functions to override movement speed. Will use the last added override. </summary>
+    /// <summary> このオブジェクトの物理演算 </summary>
+    private Rigidbody rigidbody;
+
+    /// <summary> 移動速度を最後に追加された速度に上書きする </summary>
     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
 
-
-
+    /// <summary>
+    /// 各種コンポーネントのキャッシュ
+    /// </summary>
     void Awake()
     {
-        // Get the rigidbody on this.
         rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        status = GetComponent<PlayerStatus>();
     }
 
     void FixedUpdate()
@@ -50,75 +53,113 @@ public class PlayerController : MonoBehaviour
         Debugger();
     }
 
+    /// <summary>
+    /// このキャラクターの挙動やアニメーション
+    /// </summary>
     void Movement()
     {
-        Status();
+        GetNowPlayAnim();
+        MoveStatus();
         Move();
     }
+
+    /// <summary>
+    /// オブジェクトの移動
+    /// </summary>
     void Move()
     {
-        // Get targetMovingSpeed.
+        // 移動速度の更新
         float targetMovingSpeed = status.run ? runSpeed : speed;
         if (speedOverrides.Count > 0)
         {
             targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
         }
 
-        // Get targetVelocity from input.
+        // 移動距離を計算
         Vector2 targetVelocity = new Vector2(moveX * targetMovingSpeed, moveY * targetMovingSpeed);
-        Jump();
 
-        // Apply movement.
+        // 実際にオブジェクトを動かす
+        Jump();
         rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
     }
-    void Status()
-    {
-        GetNowPlayAnim();
-        MoveStatus();
-    }
+
+    /// <summary>
+    /// 現在のアニメーション名の取得
+    /// </summary>
     private void GetNowPlayAnim()
     {
+        // アニメーションを取得
         clipInfos = animator.GetCurrentAnimatorClipInfo(1);
+        // アニメーションがNone(null)でない場合、アニメーション名を取得
         if(clipInfos.Length != 0) nowAnim = clipInfos[0].clip.name.ToString();
     }
+
+    /// <summary>
+    /// キー入力による移動速度の処理
+    /// </summary>
     private void MoveStatus()
     {
         MoveX();
         MoveY();
     }
 
+    /// <summary>
+    /// 横移動の処理
+    /// </summary>
     private void MoveX()
     {
+        // 左右どちらかのみ
         moveX = status.right ?  1 : moveX;
         moveX = status.left  ? -1 : moveX;
-        moveX = status.right & (status.flont | status.back) ? sqrtMove : moveX;
-        moveX = status.right & status.flont & status.back ? 1 : moveX;
-        moveX = status.left  & (status.flont | status.back) ? -sqrtMove : moveX;
-        moveX = status.left  & status.flont & status.back ? -1 : moveX;
-        moveX = (status.right & status.left) | (!status.right & !status.left) ? 0 : moveX;
-        moveX = status.unique ? 0 : moveX;
-    }
-    private void MoveY()
-    {
-        moveY = status.flont ?  1 : moveY;
-        moveY = status.back  ? -1 : moveY;
-        moveY = status.flont & (status.right | status.left) ? sqrtMove : moveY;
-        moveY = status.flont & status.right & status.left ? 1 : moveY;
-        moveY = status.back  & (status.right | status.left) ? -sqrtMove : moveY;
-        moveY = status.back  & status.right & status.left ? -1 : moveY;
-        moveY = (status.flont & status.back) | (!status.flont & !status.back) ? 0 : moveY;
-        moveY = status.unique ? 0 : moveY;
+
+        // 左右どちらかと上下どちらか
+        moveX = status.right & (status.flont ^ status.back) ? sqrtMove : moveX;
+        // moveX = status.right & status.flont & status.back ? 1 : moveX;
+        moveX = status.left  & (status.flont ^ status.back) ? -sqrtMove : moveX;
+        // moveX = status.left  & status.flont & status.back ? -1 : moveX;
+
+        // 左右同じ入力、またはユニーク入力
+        moveX = !(status.right ^ status.left) | status.unique ? 0 : moveX;
+        // moveX = (status.right & status.left) | (!status.right & !status.left) ? 0 : moveX;
+        // moveX = status.unique ? 0 : moveX;
     }
 
+    /// <summary>
+    /// 縦移動の処理
+    /// </summary>
+    private void MoveY()
+    {
+        // 上下どちらかのみ
+        moveY = status.flont ?  1 : moveY;
+        moveY = status.back  ? -1 : moveY;
+
+        // 上下どちらかと左右どちらか
+        moveY = status.flont & (status.right ^ status.left) ? sqrtMove : moveY;
+        // moveY = status.flont & status.right & status.left ? 1 : moveY;
+        moveY = status.back  & (status.right ^ status.left) ? -sqrtMove : moveY;
+        // moveY = status.back  & status.right & status.left ? -1 : moveY;
+
+        // 上下同じ入力、またはユニーク入力
+        moveY = !(status.flont ^ status.back) | status.unique ? 0 : moveY;
+        // moveY = (status.flont & status.back) | (!status.flont & !status.back) ? 0 : moveY;
+        // moveY = status.unique ? 0 : moveY;
+    }
+
+    /// <summary>
+    /// ジャンプの動作
+    /// </summary>
     private void Jump()
     {
         if (status.jump)
         {
+            // 上方向に力を加える
             rigidbody.AddForce(Vector3.up * 100 * jumpStrength);
-            Jumped?.Invoke();
         }
     }
 
+    /// <summary>
+    /// 対応するアニメーションの変数をセット
+    /// </summary>
     void Animation()
     {
         animator.SetFloat("Move", moveY);
@@ -128,12 +169,18 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("Attack", status.attack);
     }
 
+    /// <summary>
+    /// コライダーがヒットしたらアニメーションを再生
+    /// </summary>
     public void AddHitAnim()
     {
         animator.SetTrigger("Hit");
     }
 
-
+    /// <summary>
+    /// 現在のアニメーション名を返す
+    /// </summary>
+    /// <returns> 現在のアニメーション名 </returns>
     public string GetNowAnim() { return nowAnim; }
 
     void Debugger()
