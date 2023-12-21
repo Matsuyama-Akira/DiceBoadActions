3b05d8ae814ea180cca6cd4b741c3144398d4798
MatsuyamaAkira <matsuyama220061@eggnet.ac.jp>
2023-12-21 12:34:10 +0900

    2023/12/21 12:33 (Re upload) Script

--- Assets/MainGameFolder/Script/Battle/GroundedChecker.cs
+++ Assets/MainGameFolder/Script/Battle/GroundedChecker.cs
@@ -1,40 +1,38 @@
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EnemyIsGrounded : MonoBehaviour
+public class GroundedChecker : MonoBehaviour
 {
-    [Tooltip("Maximum distance from the ground.")]
+    [Tooltip("地面からの最大距離")]
     public float distanceThreshold = .15f;
 
-    [Tooltip("Whether this transform is grounded now.")]
+    [Tooltip("着地しているか")]
     public bool isGrounded = true;
-    /// <summary>
-    /// Called when the ground is touched again.
-    /// </summary>
+    /// <summary> 再着地時にコール </summary>
     public event System.Action Grounded;
 
-    const float OriginOffset = .001f;
+    const float OriginOffset = 0.001f;
     Vector3 RaycastOrigin => transform.position + Vector3.up * OriginOffset;
     float RaycastDistance => distanceThreshold + OriginOffset;
 
     void LateUpdate()
     {
-        // Check if we are grounded now.
+        // 着地しているかを確認
         bool isGroundedNow = Physics.Raycast(RaycastOrigin, Vector3.down, distanceThreshold * 2);
 
-        // Call event if we were in the air and we are now touching the ground.
+        // 再着地したならイベントをコール
         if (isGroundedNow && !isGrounded)
         {
             Grounded?.Invoke();
         }
 
-        // Update isGrounded.
+        // 着地情報を更新
         isGrounded = isGroundedNow;
     }
 
     void OnDrawGizmosSelected()
     {
-        // Draw a line in the Editor to show whether we are touching the ground.
+        // エディター上にRaycastを表示し、着地しているかを確認
         Debug.DrawLine(RaycastOrigin, RaycastOrigin + Vector3.down * RaycastDistance, isGrounded ? Color.white : Color.red);
     }
 }
