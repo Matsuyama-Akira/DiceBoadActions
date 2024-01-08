c745d30b0f33d9c4261db77ba0160bac017606f5
Matsuyama220061 <122959720+Matsuyama220061@users.noreply.github.com>
2024-01-09 01:08:07 +0900

    2024/01/09 01:07
    (Add)Comment

--- Assets/MainGameFolder/Script/Battle/Enemy/EnemyStates.cs
+++ Assets/MainGameFolder/Script/Battle/Enemy/EnemyStates.cs
@@ -1,42 +1,70 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyStates : MonoBehaviour
 {
+    // 必須スクリプト
     [SerializeField] BattleManagement _Manager;
     [SerializeField] BattleUIManagement _UIManager;
     [SerializeField] EnemyMove move;
-    [Range(1000, 100000), SerializeField] int startHP;
-    [Range(100, 20000), SerializeField] int startMaxStanPoint;
-    [Range(0f, 1f), SerializeField] float _dot;
-    [Range(20, 400), SerializeField] int attackDamage;
 
-    //Private states
+    // 調整可能ステータス
+    [Range(1000, 100000), SerializeField, Tooltip("スタート時のHP")] int startHP;
+    [Range(100, 20000), SerializeField, Tooltip("最初のスタンに必要な値")] int startMaxStanPoint;
+    [Range(0f, 1f), SerializeField, Tooltip("視野角")] float _dot;
+    [Range(20, 400), SerializeField, Tooltip("攻撃力")] int attackDamage;
+
+    // 非表示ステータス
+    /// <summary> 現在のHP </summary>
     private int nowHP;
+    /// <summary> 現在のスタン値 </summary>
     private int stanPoint;
+    /// <summary> 現在のスタンに必要な値 </summary>
     private int maxStanPoint;
+
+    /// <summary> 死亡判定 </summary>
     public bool isDie { get; private set; }
+    /// <summary> スタン判定 </summary>
     public bool isStan { get; private set; }
+    /// <summary> ダメージ判定 </summary>
     public bool isDamage { get; private set; }
+    /// <summary> 死亡アニメーション終了判定 </summary>
     public bool isDieAnimEnd => isDie & move.isNullAnim;
 
+    // 数値の加減算
+    /// <summary> 受けたダメージ分HPを減らす </summary>
     public void AddHP(int damage) { nowHP -= damage; }
+    /// <summary> スタン値を増やす </summary>
     public void AddStanPoint(int point) { stanPoint += point; }
 
+    // ステータスの取得
+    /// <summary> 現在のHPを取得 </summary>
     public int GetNowHP() { return nowHP; }
+    /// <summary> 現在のスタン値を取得 </summary>
     public int GetStanPoint() { return stanPoint; }
+    /// <summary> 視野角を取得 </summary>
     public float GetDot() { return _dot; }
+    /// <summary> 攻撃力を取得 </summary>
     public int GetAttackDamage() { return attackDamage; }
 
+    /// <summary>
+    /// ダメージ量の表示
+    /// </summary>
+    /// <param name="col"></param>
+    /// <param name="damage"></param>
     public void DamageUI(Collider col, int damage)
     {
+        // マネージャーへ変数の受け渡し
         _UIManager.DamageUI(col, damage);
         _Manager.AddAllDamage(damage);
     }
 
     private void Awake()
     {
+        // セットアップ
         _Manager = GameObject.Find("BattleManager").GetComponent<BattleManagement>();
         _UIManager = GameObject.Find("BattleManager").GetComponent<BattleUIManagement>();
+
+        // ステータスの初期化
         nowHP = startHP;
         maxStanPoint = startMaxStanPoint;
         isDamage = false;
@@ -45,20 +73,13 @@ public class EnemyStates : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Damage();
-        Die();
-        Stan();
-    }
-    void Damage()
-    {
+        // ダメージを受けたかの判定
         isDamage = startHP > nowHP;
-    }
-    void Die()
-    {
+
+        // HPが無くなったかの判定
         isDie = nowHP <= 0;
-    }
-    void Stan()
-    {
+
+        // スタン値が必要スタン値を超えたかの判定
         if (isStan) { stanPoint = 0; maxStanPoint = (int)(maxStanPoint * 1.5); }
         isStan = stanPoint >= maxStanPoint;
     }
--- Assets/MainGameFolder/Script/Battle/Param/BowParamTable.cs
+++ Assets/MainGameFolder/Script/Battle/Param/BowParamTable.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "BowParamTable", menuName = "CreateParamData/CreateBowParam")]
@@ -10,21 +10,34 @@ public class BowParamTable : ScriptableObject
 [System.Serializable]
 public class BowSellection
 {
+    /// <summary> 武器のレベル </summary>
     public enum Level
     {
         Common  = 0,
         Rare    = 1,
         Unique  = 2,
     }
+
+    /// <summary> 矢の属性 </summary>
     public enum BowType
     {
         Noumal,
         Penetrate,
         Diffusion
     }
+
+    /// <summary> この武器のレベル選択 </summary>
     public Level level;
+
+    /// <summary>  </summary>
     public BowType type;
+
+    /// <summary>  </summary>
     [Range(1, 100)] public int baseDamage;
+
+    /// <summary>  </summary>
     [Range(1.0f, 2.0f)] public float magunification;
+
+    /// <summary>  </summary>
     [Range(0, 100)] public int criticalRange;
 }
